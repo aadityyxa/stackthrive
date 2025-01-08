73d6bba13caf6740f111ff63019bf191aa339286
aadityyxa <aditya.oza256@gmail.com>
2025-01-08 05:28:49 -0800

    three pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ThankYouComponent } from './thank-you/thank-you.component';
+import { Sign } from 'crypto';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path:'sign-up', component:SignUpComponent},
+    {path:'thank-you', component:ThankYouComponent}
+];
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+  onSubmit() {
+    console.log('Form Submitted')
+  }
+}
